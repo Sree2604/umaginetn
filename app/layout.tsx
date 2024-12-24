262e2a2071454e2a06263ae513b8aeb5a2c9b318
AravindCodesAllDay <aravindsiva1509@gmail.com>
2024-12-24 20:13:52 +0530

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,12 @@ export const metadata: Metadata = {
   description: "Embark on a journey of Transformation",
 };
 
+export const pushToDataLayer = (event: object) => {
+  if (typeof window !== "undefined" && window.dataLayer) {
+    window.dataLayer.push(event);
+  }
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -14,6 +20,17 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','AW-16454344405');
+              `,
+          }}
+        />
         <link
           rel="preload"
           href="/fonts/Fontspring-DEMO-neometricalt-black.otf"
@@ -29,7 +46,17 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
       </head>
-      <body className="font-myraidpro">{children}</body>
+      <body className="font-myraidpro">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=AW-16454344405"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
