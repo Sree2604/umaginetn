771825354ac0228a68c09977e8ac3fe80b00d30f
AravindCodesAllDay <aravindsiva1509@gmail.com>
2024-12-24 20:20:43 +0530

    ts datatype

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,29 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
+// Define metadata
 export const metadata: Metadata = {
   title: "UmagineTN 2025 Experience",
   description: "Embark on a journey of Transformation",
 };
 
+// Define the DataLayerEvent type
 interface DataLayerEvent {
   event: string;
-  [key: string]: any;
+  category?: string;
+  action?: string;
+  [key: string]: string | number | boolean | undefined;
 }
 
+// Push events to the dataLayer
 export const pushToDataLayer = (event: DataLayerEvent) => {
   if (typeof window !== "undefined" && window.dataLayer) {
     window.dataLayer.push(event);
   }
 };
 
+// RootLayout component
 export default function RootLayout({
   children,
 }: {
@@ -25,17 +32,25 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        <script
+        {/* Google Tag Manager Script */}
+        <Script
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
+                (function(w,d,s,l,i){
+                  w[l]=w[l]||[]; 
+                  w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'}); 
+                  var f=d.getElementsByTagName(s)[0], 
+                  j=d.createElement(s), 
+                  dl=l!='dataLayer'?'&l='+l:''; 
+                  j.async=true; 
+                  j.src='https://www.googletagmanager.com/gtm.js?id=' + i + dl; 
+                  f.parentNode.insertBefore(j,f);
                 })(window,document,'script','dataLayer','AW-16454344405');
               `,
           }}
         />
+        {/* Preload Fonts */}
         <link
           rel="preload"
           href="/fonts/Fontspring-DEMO-neometricalt-black.otf"
@@ -52,6 +67,7 @@ export default function RootLayout({
         />
       </head>
       <body className="font-myraidpro">
+        {/* Google Tag Manager Noscript */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=AW-16454344405"
